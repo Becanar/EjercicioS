fc84a6a76763fa544fe8f148ef90fde6bb70d7e9
Becanar <116558378+Becanar@users.noreply.github.com>
2024-11-01 14:06:36 +0100

    Login funciona, añado estructura de carpetas y sql

---
+++ src/main/java/com/example/ejercicios/model/Animal.java
@@ -0,0 +1,4 @@
+package com.example.ejercicios.model;
+
+public class Animal {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.example.ejerciciol {
+module com.example.ejercicios {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql.rowset;
